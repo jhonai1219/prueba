62845c8de2f063a88d80d77645b743c90b4f1230
jhonai1219 <85982393+jhonai1219@users.noreply.github.com>
2021-06-16 21:16:51 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
-    <title>Iniciar sesión</title>
+    <title>Iniciar sesión para ventilar</title>
 
     	
     <script src='https://www.google.com/recaptcha/api.js?hl=es'></script>
